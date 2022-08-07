9c852422df21fbfca6c88ba27fdf36736084f1da
mattseah17 <matthewseah17@gmail.com>
2022-08-07 13:03:27 +0800

    added login and addplant

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Login = () => {
-    return (
-        <>
-            <h1>Login to your account</h1>
-        </>
-    );
+  //states for login
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //handling changes
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+  
+  return (
+    <>
+      <div>
+        <h1>Login to your account</h1>
+      </div>
+      <form onSubmit={handleLogin}>
+        <label>Email:</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Your email"
+          value={email}
+          onChange={handleEmail}
+          required
+        />
+        <br></br>
+        <label>Password:</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="Your password"
+          value={password}
+          onChange={handlePassword}
+          required
+        />
+        <div>
+          <button id="submit" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Login;
