38407a5df79d7fe94f5e6b2f48b2329874fe9fac
mattseah17 <matthewseah17@gmail.com>
2022-08-08 15:16:56 +0800

    changed ports in .json

--- server/authserver.js
+++ server/authserver.js
@@ -8,12 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 const authroute = require("./routes/authroute");
 app.use("/auth", authroute);
 
-
-const PORT = process.env.AUTH_PORT || 5003;
-app.listen(PORT, () => {
-  console.log(`listening to port ${PORT}`);
+const AUTH_PORT = process.env.AUTH_PORT || 5003;
+app.listen(AUTH_PORT, () => {
+  console.log(`listening to port ${AUTH_PORT}`);
 });
