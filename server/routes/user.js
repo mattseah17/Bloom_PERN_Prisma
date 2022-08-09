213b8beb40c43d70ba1b425bd5a0ba312f1b8a57
mattseah17 <matthewseah17@gmail.com>
2022-08-09 08:48:33 +0800

    deleted authroute and edited user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -38,6 +38,79 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//User Login
+router.post("/login", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await prisma.user.findFirst({ where: { email: email } });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ status: "error", message: "not authorised" });
+    }
+
+    const result = await bcrypt.compare(req.body.password, user.hash);
+    if (!result) {
+      console.log("email or password error");
+      return res.status(401).json({ status: "error", message: "Login failed" });
+    }
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+
+    const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
+      expiresIn: "20m",
+      jwtid: uuidv4(),
+    });
+
+    const refresh = jwt.sign(payload, process.env.REFRESH_SECRET, {
+      expiresIn: "30d",
+      jwtid: uuidv4(),
+    });
+
+    const response = { access, refresh };
+    res.json(response);
+  } catch (error) {
+    console.log("POST /login", error);
+    res.status(400).json({ status: "error", message: "login failed" });
+  }
+});
+
+//Refresh token
+router.post("/refresh", (req, res) => {
+  try {
+    const decoded = jwt.verify(req.body.refresh, process.env.REFRESH_SECRET);
+    console.log(decoded);
+
+    const payload = {
+      id: decoded.id,
+      email: decoded.email,
+    };
+
+    const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
+      expiresIn: "20m",
+      jwtid: uuidv4(),
+    });
+
+    const response = { access };
+    res.json(response);
+  } catch (error) {
+    console.log("POST/refresh", error);
+    res.status(401).json({
+      status: "error",
+      message: "unauthorised",
+    });
+  }
+});
+
+// router.delete("/logout", (req, res) => {
+//   refreshTokens = refreshTokens.filter((token) => token != req.body.token);
+//   res.status(204);
+//   console.log(refreshTokens);
+// });
+
 //Get user stuff
 router.get("/:id", auth, async (req, res) => {
   try {
