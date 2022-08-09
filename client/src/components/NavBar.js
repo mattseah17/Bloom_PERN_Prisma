90879c9dcacf2eeb314df134aa781e716ac9465a
mattseah17 <matthewseah17@gmail.com>
2022-08-09 11:11:14 +0800

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,24 +1,57 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-
+import React, { useContext } from "react";
+import ReactContext from "../context/reactcontext";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const reactCtx = useContext(ReactContext);
+
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+
+  const handleLogoutClick = () => {
+    reactCtx.setLoginState(false);
+    reactCtx.setAccess("");
+    navigate("/");
+  };
 
   return (
     <>
       <header>
         <nav>
-          <ul>
-            <li>
-              <NavLink to="/user">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/user/add">Add Plant</NavLink>
-            </li>
-            <li>
-              <NavLink to="/user/update">Profile</NavLink>
-            </li>
-          </ul>
+          {reactCtx.loginState ? (
+            <>
+              <ul>
+                <li>
+                  <NavLink to="/user">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/update">Profile</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/add">Add Plant</NavLink>
+                </li>
+                <div>
+                  <button onClick={handleLogoutClick}>Log Out</button>
+                </div>
+              </ul>
+            </>
+          ) : (
+            <>
+              <div>
+                <button onClick={handleLoginClick}>Login</button>
+              </div>
+              <div>
+                <button onClick={handleRegisterClick}>Register</button>
+              </div>
+            </>
+          )}
         </nav>
       </header>
     </>
