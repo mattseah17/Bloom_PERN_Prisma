205a4715062e1b5a35cdd53f43d54863b1e167e3
mattseah17 <matthewseah17@gmail.com>
2022-08-09 11:16:42 +0800

    Update Landing.js

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,33 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
-  const navigate = useNavigate();
-  const handleLoginClick = () => {
-    navigate("/login");
-  };
-
-  const handleRegisterClick = () => {
-    navigate("/register");
-  };
   return (
     <>
-      <header>
+      <div>
         <div>
-          <div>
-            <h1>Bloom</h1>
-          </div>
-          <div>
-            <p>Blah blah</p>
-          </div>
+          <h1>Bloom</h1>
         </div>
         <div>
-          <button onClick={handleLoginClick}>Login</button>
+          <p>Blah blah</p>
         </div>
-        <div>
-          <button onClick={handleRegisterClick}>Register</button>
-        </div>
-      </header>
+      </div>
     </>
   );
 };
