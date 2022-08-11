57ce00fab9e35f9e439643acfb464e6a24182969
mattseah17 <matthewseah17@gmail.com>
2022-08-11 13:23:51 +0800

    css updates

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -24,34 +24,55 @@ const NavBar = () => {
   return (
     <>
       <header>
-        <nav>
-          {reactCtx.loginState ? (
-            <>
-              <ul>
-                <li>
-                  <NavLink to="/home">Home</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/update">Profile</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/add">Add Plant</NavLink>
-                </li>
-                <div>
-                  <button onClick={handleLogoutClick}>Log Out</button>
+        <nav className="bg-green-800 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+          <div className="container flex flex-wrap justify-between items-center mx-auto">
+            <img src="" class="mr-3 h-6 sm:h-9" alt="Bloom Logo" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Bloom
+            </span>
+
+            {reactCtx.loginState ? (
+              <>
+                <div
+                  className="hidden w-full md:block md:w-auto"
+                  id="navbar-default"
+                >
+                  <ul>
+                    <li>
+                      <NavLink to="/home" className="flex mb-2 hover:scale-105">
+                        Home
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/update"
+                        className="flex mb-2 hover:scale-105"
+                      >
+                        Profile
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/add" className="flex mb-2 hover:scale-105">
+                        Add Plant
+                      </NavLink>
+                    </li>
+                    <div>
+                      <button onClick={handleLogoutClick}>Log Out</button>
+                    </div>
+                  </ul>
                 </div>
-              </ul>
-            </>
-          ) : (
-            <>
-              <div>
-                <button onClick={handleLoginClick}>Login</button>
-              </div>
-              <div>
-                <button onClick={handleRegisterClick}>Register</button>
-              </div>
-            </>
-          )}
+              </>
+            ) : (
+              <>
+                <div>
+                  <button onClick={handleLoginClick}>Login</button>
+                </div>
+                <div>
+                  <button onClick={handleRegisterClick}>Register</button>
+                </div>
+              </>
+            )}
+          </div>
         </nav>
       </header>
     </>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/components/*.{js,jsx}",
     "./src/App.js",
     "./public/index.html",
+    "node_modules/flowbite-react/**/*.{js,jsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
