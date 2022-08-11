23c0a081d1a08da78d1cbc478935447c7b21ae43
mattseah17 <matthewseah17@gmail.com>
2022-08-11 10:06:12 +0800

    tailwind added

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/*.{js,jsx}",
+    "./src/App.js",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
